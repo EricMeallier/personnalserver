9a9480edbc7315728b0270245716e8b97a809f5d
EricMeallier <eric@meallier.fr>
2026-03-04 14:10:45 +0100

    Add thunderbird backup script

---
+++ documentation/thunderbirdBackup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+timestamp=`date +%Y%m%d_%H%M`
+#timestamp='last'
+targetDir='/home/eric/pCloudDrive/BackupPC/MailThunderbird'
+targetName=${timestamp=}.thunderbird
+profileToBackup='EricNew2'
+
+
+# Arret du soft
+PID=`ps -ef |grep 'thunderbird ' | grep parentPid | tail -1 | sed 's/.*parentPid \([^ ]*\).*/\1/'`
+kill $PID
+
+# Archivage des données
+cd ~/.thunderbird
+tar cvf ${targetName}.tar ${profileToBackup}
+
+# Relance du soft
+thunderbird&
+
+# Compression + déplacement
+7z a -m0=lzma2 -mx9 ${targetName}.7z ${targetName}.tar
+rm ${targetName}.tar
+mv ${targetName}.7z ${targetDir}
+
+exit $?
